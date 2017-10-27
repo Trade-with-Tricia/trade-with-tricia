843ae32f0dcbf27096b4c6e8dfa6bd94a59c27d8
rayner18 <rayner18@up.edu>
2017-10-26 19:30:51 -0700

    First commit for Twilio branch.

--- src/main/java/com/tradewithtricia/App.java
+++ src/main/java/com/tradewithtricia/App.java
@@ -1,13 +1,22 @@
 package com.tradewithtricia;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class App {
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "AC8e82966026ad06984e2438fd3257730c";
+    public static final String AUTH_TOKEN = "c0ef4708432641607a7d95e74bede0e4";
+    public static final String AVAYA = "+12088632997";
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        Message message = Message.creator(new PhoneNumber("+19712010322"),
+                new PhoneNumber("+15034448774"),
+                "This is the ship that made the Kessel Run in fourteen parsecs?").create();
+
+        System.out.println(message.getSid());
     }
 }
